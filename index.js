0dca4feb1c61fc5fbe4fff5e679326e4cc85b7e6
loganpowell <loganp@tepper.cmu.edu>
2019-10-15 14:54:34 -0400

    tries another

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const http = require('http')
 const { PORT = 3000 } = process.env
 
 http.createServer((req, res) => {
-  res.end('Hello World from Node.js\n')
+  res.end('Hello butthole from Node.js\n')
 }).listen(PORT)
